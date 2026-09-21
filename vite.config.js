46898c379987b2c428345b02a16c3f735e41c8e9
Andreas Lindström <andreas.lindstrom@gmail.com>
2026-09-21 19:33:32 +0200

    Add build script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
+  base: repositoryName ? `/${repositoryName}/` : '/',
   plugins: [vue()],
   server: {
     port: 3000
